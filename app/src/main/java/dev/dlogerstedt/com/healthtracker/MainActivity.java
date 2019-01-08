60e0496e3d6b47423453fc2ff8de9457d97d1dff
Daniel Logerstedt <dl@Daniels-MBP-2.localdomain>
2019-01-08 12:52:32 -0800

    started work on stopwatch, found solution for building a basic timer on StackOverflow. URL included in comment at top of main activity

--- app/src/main/java/dev/dlogerstedt/com/healthtracker/MainActivity.java
+++ app/src/main/java/dev/dlogerstedt/com/healthtracker/MainActivity.java
@@ -1,16 +1,47 @@
 package dev.dlogerstedt.com.healthtracker;
 
+import android.os.Handler;
+import android.support.annotation.WorkerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+
+/*
+    Stopwatch solution was found at https://stackoverflow.com/questions/4597690/android-timer-how-to and adapted from the best answer for how to create a timer.
+ */
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView timerView;
+    long startTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerView = (TextView) findViewById(R.id.stop_watch_time);
+        final Button startStopButton = findViewById(R.id.timer_start);
+
+        startStopButton.setText("Start");
+        startStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startStopButton.getText().equals("Stop")) {
+                    stopWatchHandler.removeCallbacks(stopWatchRunner);
+                    startStopButton.setText("Start");
+
+                } else {
+                    startTime = System.currentTimeMillis();
+                    stopWatchHandler.postDelayed(stopWatchRunner, 0);
+                    startStopButton.setText("Stop");
+                }
+
+            }
+        });
     }
 
     public void onButtonClick(View v) {
@@ -20,5 +51,33 @@ public class MainActivity extends AppCompatActivity {
         String theString = "" + theCount;
         countDisplay.setText(theString);
         System.out.println(theString);
+    }
+
+    Handler stopWatchHandler = new Handler();
+    Runnable stopWatchRunner = new Runnable() {
+        @Override
+        public void run() {
+            long millis = (System.currentTimeMillis() - startTime);
+            int seconds = (int)(millis/1000);
+            int minutes = seconds/60;
+            int hours = minutes/60;
+
+            millis = millis%1000;
+            seconds = seconds%60;
+            minutes = minutes%60;
+            hours = hours%24;
+
+            timerView.setText(String.format("%1d:%2d:%3d", hours, minutes, seconds));
+            stopWatchHandler.postDelayed(this, 500);
+
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopWatchHandler.removeCallbacks(stopWatchRunner);
+        Button startStopButton = findViewById(R.id.timer_start);
+        startStopButton.setText("Start");
     }
 }
